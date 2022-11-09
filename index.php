ed52616abda4f4357bf0e12085b599d5acc6127a
Ameer-Ali-Jamali <ameer.ali.jamali886@gmail.com>
2022-11-09 19:19:00 +0500

    cleared some mistakes

--- index.php
+++ index.php
@@ -1,53 +1,57 @@
-<!DOCTYPE html>
-<html lang="en">
+Introduction to OOPS
+OOP stands for Object-Oriented Programming.
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
+Object-oriented programming is a core of PHP Programming, which is used for designing a program using classes and
+objects that contain both data and functions.
 
 
-    <?php
+Advantages of OOPS
+Programs are faster to execute.
+Better code reusability.
+Easy to understand code structure.
+Very easy to partition the work in a project based on objects.
+Easy to make fully reusable programs with less code and less development time.
 
-    // Classes
-    // Classes are the blueprint of objects. Classes hold their own data members and member functions, which can be accessed and used by creating an instance of that class. In PHP class is defined using the class keyword, followed by the name of classes and curly braces ({}). 
+Disadvantages of OOPS
+OOPS cannot be applied everywhere as it is not a universal language.
+Need to have a good understanding of classes and objects to apply OOPS.
+It takes a lot of effort to create.
+It is slower than other programs.
+OOPS programs are large in size when compared to normal programs.
 
-    // Example:
+
+Classes
+Classes are the blueprint of objects. Classes hold their own data members and member functions, which can be accessed and used by creating an instance of that class. In PHP class is defined using the class keyword, followed by the name of classes and curly braces ({}).
+
+Example:
 
 
+<?php
+class class_name
+{
+    // code goes here...
+}
+?>
 
-    class class_name
-    {
-        // code goes here...
-    }
+Creating a Class in PHP
+We will create a class Employee where we will have two properties and two methods for setting and getting the property.
 
-    // Creating a Class in PHP
-    // We will create a class Employee where we will have two properties and two methods for setting and getting the property.
+<?php
+class Employee
+{
+    // Properties
+    public $name;
+    public $surname;
 
-    class Employee
-    {
-        // Properties
-        public $name;
-        public $surname;
-    }
     // Methods
     function set_name($name)
     {
-        // $this->name = $name;
+        $this->name = $name;
     }
     function get_name()
     {
-        // return $this->name;
+        return $this->name;
     }
-
-    //    Note: The $this keyword refers to the current object, and is only available inside methods.
-
-    ?>
-
-</body>
-
-</html>
+}
+?>
+Note: The $this keyword refers to the current object, and is only available inside methods.
